a48a0d7ffa7ca0e5f468ab6c89b7e9d009b79e89
cadu <andradecadu14@gmail.com>
2024-12-02 18:29:38 -0300

    first commit

---
+++ consumo.cs
@@ -0,0 +1,16 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int X = int.Parse(Console.ReadLine());
+        double Y = double.Parse(Console.ReadLine());
+
+        double consumoMedio = X / Y;
+
+        Console.WriteLine(consumoMedio.ToString("F3") + " km/l");
+
+    }
+
+}
